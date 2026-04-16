d964f224bfdecbb3f0add0a8cac3f2395f858a7f
Ryousei Shakadou <shakadou.515@gmail.com>
2026-04-17 00:26:20 +0900

    Changed CAN data reception to CANManager management.

--- rbms.h
+++ rbms.h
@@ -2,13 +2,14 @@
 #define INCLUDED_rbms_H
 
 #include "mbed.h"
+#include "CANManager.h"
 
-class rbms {
+class rbms : public CANManager{
     public:
         rbms(CAN &can,bool motor_type,int motor_num);
-        int rbms_send(int* moter);
+        bool handle_message(const CANMessage &msg) override;
+        int rbms_send(int* motor);
         void rbms_read(CANMessage &msg, short *rotation,short *speed);
-        void can_read();
         float pid(float T,short rpm_now, short set_speed,float *delta_rpm_pre,float *ie,float KP=25,float KI=10, float KD=0);
         void spd_control(int* set_speed,int* motor);
         
